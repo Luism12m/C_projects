ac1a49084346884d73a331b74aafde94d1e17d21
root <luiscarloszipacogaria@gmail.com>
2024-07-26 19:02:03 +0000

    aplicacion reglas de compilacion ciclos

--- basics/1-main.c
+++ basics/1-main.c
@@ -10,14 +10,14 @@
 int main (void){
 
   int count = 0;
-   while (count <=5){
+   while (count <= 5){
     printf("iteracion [%d] hola mundo\n",count);
     count = count + 1;
   }
    
    
-   for (int count = 0; count <= 5; count++){
+   for (count = 0; count <= 5; count++){
      printf("iteracion [%d] hola mundo con for\n",count);
    }
-
+   return 0;
 }
